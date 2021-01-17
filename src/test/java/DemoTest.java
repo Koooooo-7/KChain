1513115c5e3a22a3c52f25f5e5a6c22b4c0f3187
Koy <koy@ko8e24.top>
2021-01-17 08:49:48 +0800

    test(app): main cover.

---
+++ src/test/java/DemoTest.java
@@ -0,0 +1,15 @@
+import demo.App;
+import org.junit.Test;
+
+/**
+ * @author Koy  https://github.com/Koooooo-7
+ * @Description
+ */
+
+public class DemoTest {
+
+    @Test
+    public void runMain(){
+        App.main(new String[]{});
+    }
+}
