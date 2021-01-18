4c469a8e686dfe6a04896338ba5facf8fb4f8c15
Koy <koy@ko8e24.top>
2021-01-18 11:32:35 +0800

    add(test): add test cases.

--- src/test/java/PropertiesCheckChainTest.java
+++ src/test/java/PropertiesCheckChainTest.java
@@ -1,3 +1,5 @@
+import com.google.common.collect.ListMultimap;
+import com.google.common.collect.Lists;
 import core.ChainContext;
 import core.DataWrapper;
 import core.IChain;
@@ -11,7 +13,9 @@ import service.map.MapDataWrapper;
 import service.map.MapRuleContext;
 import service.map.chain.MapPropertiesCheckChain;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.mockito.Mockito.mock;
@@ -34,19 +38,49 @@ public class PropertiesCheckChainTest {
     }
 
     @Test
-    public void testMapChain() {
+    public void testMapChainOnPredicated() {
         Map<String, Object> map = mock(HashMap.class);
         when(map.get("name")).thenReturn("");
         when(map.get("age")).thenReturn(24);
+
         MapRuleContext ruleContext = new MapRuleContext(RuleStrategy.FULL_CHECK);
-
         MapDataWrapper mapDataWrapper = new MapDataWrapper(map, ruleContext);
-        Assert.assertNull(mapDataWrapper.get(""));
-        Assert.assertNull(mapDataWrapper.getString(""));
-        Assert.assertNull(mapDataWrapper.getLong(""));
-        mapPropertiesCheckChain.test(ctx, mapDataWrapper);
-        Map<String, String> result = ruleContext.getResult();
-        Assert.assertEquals(Rule.NOT_EMPTY.name(), result.get("name"));
 
+        Assert.assertNull(mapDataWrapper.get(""));
+        Assert.assertEquals("", mapDataWrapper.getString(""));
+        Assert.assertNull(mapDataWrapper.getLong(""));
+
+        mapPropertiesCheckChain.test(ctx, mapDataWrapper);
+        ListMultimap<String, String> result = ruleContext.getResult();
+        Assert.assertTrue(result.get("name").contains(Rule.NOT_EMPTY.name()));
+
+    }
+
+
+    @Test
+    public void testMapChainOnFunction() {
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("name", "");
+        map.put("age", 24);
+        HashMap<String, Object> map2 = new HashMap<>();
+        map2.put("name", "Kobe");
+        map2.put("age", 24);
+
+        MapRuleContext ruleContext = new MapRuleContext(RuleStrategy.FULL_CHECK);
+        MapRuleContext ruleContext2 = new MapRuleContext(RuleStrategy.FULL_CHECK);
+        MapDataWrapper mapDataWrapper = new MapDataWrapper(map, ruleContext);
+        MapDataWrapper mapDataWrapper2 = new MapDataWrapper(map2, ruleContext2);
+        List<MapDataWrapper> dataWrappers = Lists.newArrayListWithCapacity(2);
+        dataWrappers.add(mapDataWrapper);
+        dataWrappers.add(mapDataWrapper2);
+        ChainContext ctx = new ChainContext();
+        MapPropertiesCheckChain mapPropertiesCheckChain = new MapPropertiesCheckChain();
+        mapPropertiesCheckChain.test(ctx, mapDataWrapper);
+        mapPropertiesCheckChain.apply(ctx, dataWrappers);
+
+        Assert.assertTrue(mapDataWrapper.getRuleContext().getResult().get("name").contains(Rule.NOT_EMPTY.name()));
+        Assert.assertTrue(mapDataWrapper.getRuleContext().getResult().get("age").contains(Rule.DUPLICATED.name()));
+        Assert.assertFalse(mapDataWrapper2.getRuleContext().getResult().get("name").contains(Rule.NOT_EMPTY.name()));
+        Assert.assertTrue(mapDataWrapper2.getRuleContext().getResult().get("age").contains(Rule.DUPLICATED.name()));
     }
 }
