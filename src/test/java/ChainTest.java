cae7b64f7d5ea59eebd563c57d5579a8c77d2984
Koy <koy@ko8e24.top>
2021-01-22 20:51:31 +0800

    add(test): add test cases.

--- src/test/java/ChainTest.java
+++ src/test/java/ChainTest.java
@@ -1,7 +1,13 @@
 import core.ChainContext;
 import demo.DefaultPropertiesCheckChain;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Assert;
 import org.junit.Test;
 import rule.RuleStrategy;
+import rule.TrdConsumer;
+import rule.TrdFunction;
+
+import java.util.function.Function;
 
 /**
  * @author Koy  https://github.com/Koooooo-7
@@ -20,5 +26,25 @@ public class ChainTest {
     public void testChainContext() {
         ChainContext chainContext = new ChainContext(RuleStrategy.FAST_FAILED);
         chainContext.setRuleStrategy(RuleStrategy.FAST_FAILED);
+    }
+
+    @Test
+    public void testTrdConsumer() {
+        TrdConsumer<String, String, String> consumer1 = (s1, s2, s3) -> {
+        };
+        TrdConsumer<String, String, String> consumer2 = (s1, s2, s3) -> {
+        };
+
+        consumer1.andThen(consumer2).accept("1", "2", "3");
+
+    }
+
+    @Test
+    public void testTrdFunction() {
+        TrdFunction<String, String, String, String> function1 = (s1, s2, s3) -> s1;
+        Function<String, String> function2 = s1 -> s1;
+        String result = function1.andThen(function2).apply("1", "2", "3");
+        Assert.assertTrue(StringUtils.equals("1", result));
+
     }
 }
