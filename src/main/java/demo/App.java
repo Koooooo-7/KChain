b84a43098ffa53b974c0b427439e341dd4694805
Koy <koy@ko8e24.top>
2021-01-23 19:35:25 +0800

    add(demo): add entity demo.

--- src/main/java/demo/App.java
+++ src/main/java/demo/App.java
@@ -3,6 +3,10 @@ package demo;
 import com.google.common.collect.Lists;
 import core.ChainContext;
 import rule.RuleStrategy;
+import service.entity.EntityDataWrapper;
+import service.entity.EntityRuleContext;
+import service.entity.User;
+import service.entity.chain.EntityPropertiesCheckChain;
 import service.map.MapDataWrapper;
 import service.map.MapRuleContext;
 import service.map.chain.MapPropertiesCheckChain;
@@ -21,6 +25,7 @@ public class App {
 
     public static void main(String[] args) {
         demoOnMap();
+        demoOnEntity();
     }
 
 
@@ -47,6 +52,7 @@ public class App {
         MapDataWrapper mapDataWrapper = new MapDataWrapper(map, ruleContext);
 
         mapPropertiesCheckChain.test(ctx, mapDataWrapper);
+
         MapRuleContext ruleContext2 = new MapRuleContext(ctx.getRuleStrategy());
         MapDataWrapper mapDataWrapper2 = new MapDataWrapper(map2, ruleContext2);
         List<MapDataWrapper> dataWrappers = Lists.newArrayListWithCapacity(2);
@@ -57,5 +63,28 @@ public class App {
 
         System.out.println(mapDataWrapper.getRuleContext().getResult());
         System.out.println(mapDataWrapper2.getRuleContext().getResult());
+    }
+
+    public static void demoOnEntity() {
+        User user = new User("", 24);
+        User user2 = new User("Kobe", 24);
+
+        EntityPropertiesCheckChain entityPropertiesCheckChain = new EntityPropertiesCheckChain();
+        ChainContext ctx = new ChainContext(RuleStrategy.FULL_CHECK);
+        EntityRuleContext ruleContext = new EntityRuleContext(ctx.getRuleStrategy());
+        EntityDataWrapper entityDataWrapper = new EntityDataWrapper(user, ruleContext);
+
+        entityPropertiesCheckChain.test(ctx, entityDataWrapper);
+
+        EntityRuleContext ruleContext2 = new EntityRuleContext(ctx.getRuleStrategy());
+        EntityDataWrapper entityDataWrapper2 = new EntityDataWrapper(user2, ruleContext2);
+        List<EntityDataWrapper> dataWrappers = Lists.newArrayListWithCapacity(2);
+        dataWrappers.add(entityDataWrapper);
+
+        dataWrappers.add(entityDataWrapper2);
+        entityPropertiesCheckChain.apply(ctx, dataWrappers);
+
+        System.out.println(entityDataWrapper.getRuleContext().getResult());
+        System.out.println(entityDataWrapper2.getRuleContext().getResult());
     }
 }
