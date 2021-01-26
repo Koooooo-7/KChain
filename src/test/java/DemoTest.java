d55cb68403306ce85e4ba6a74d514e5fc402bd3b
Koy <koy@ko8e24.top>
2021-01-26 11:36:56 +0800

    add(test): add entity test.

--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -1,6 +1,9 @@
 import demo.App;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import service.entity.User;
 
 /**
  * @author Koy  https://github.com/Koooooo-7
@@ -11,7 +14,17 @@ public class DemoTest {
 
     @Test
     @DisplayName("test Demos")
-    public void testDemoApp(){
+    public void testDemoApp() {
         App.main(new String[]{});
+    }
+
+    @Test
+    public void testUserEntity() {
+        User user = new User("", 0);
+        user.setName("Kobe");
+        user.setAge(24);
+        Assertions.assertEquals("Kobe", user.getName());
+        Assertions.assertEquals(24, user.getAge());
+        Assertions.assertTrue(StringUtils.isNotEmpty(user.toString()));
     }
 }
